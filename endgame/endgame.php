9c774e737da18535aa9c909083a41daf7932c66c
samhitag96 <48071643+samhitag96@users.noreply.github.com>
2019-10-30 13:40:27 -0400

    Add files via upload

---
+++ endgame/endgame.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head> 
+    <link rel="stylesheet" type="text/css" href="endgame.css">
+        <title> GAME OVER </title>
+</head>
+<body>
+    <div id = endgame>
+        <img src ="gameover.jpg" alt='game over'>    
+</div>
+    <div id = "main">
+    <form action = "leaderboard.php" method="post">
+    <span><input type = "submit" class = "button" value="Leaderboard"> </span>
+    </form>
+
+    <form action = "login.php" method="post">
+    <div id = button1><span><input type = "submit" class = "button1" value="Login Page"> </span> </div>
+</div>    
+</form>
+    </html>
