d37b2125491b20776d499ba96477a0d017d65e3d
samhitag96 <48071643+samhitag96@users.noreply.github.com>
2019-10-29 20:52:12 -0400

    Update solution.php

--- Math/solution.php
+++ Math/solution.php
@@ -1,5 +1,4 @@
 <?php
-
 	$user_answer = $_POST['user_answer'];
 	$answer = $_POST['answer'];
 	$correct = "";
@@ -7,7 +6,7 @@
 	echo "Your answer: " . $user_answer . "<br>";
 	echo "Correct answer: " . $answer . "<br>";
 	
-	
+	$correct = 0;
 	if ($user_answer == $answer) {
 		echo "correct! <br>";
 		$correct++;
@@ -15,6 +14,6 @@
 	else {
 		echo "incorrect <br>";
 	}
-		
+	echo "You got $correct correct answers";
 		
 ?>
