fc88279739f1f9321ba77057941a998e63c85338
csanders29 <33591356+csanders29@users.noreply.github.com>
2019-10-29 20:36:39 -0400

    Create solution.php

---
+++ Math/solution.php
@@ -0,0 +1,20 @@
+<?php
+
+	$user_answer = $_POST['user_answer'];
+	$answer = $_POST['answer'];
+	$correct = "";
+	
+	echo $user_answer;
+	echo $answer;
+	
+	/*
+	if ($user_answer == $answer) {
+		echo "correct!";
+		$correct++;
+	}
+	else {
+		echo "incorrect";
+	}
+		
+		*/
+?>
